f1d2fd6f132f5d3aa50ba34648d3c425b9b70a8e
ffiorentino12 <francesco.fiorentino@finconsgroup.com>
2022-07-21 16:16:02 +0200

    2 files modified

--- src/app/store/actions.ts
+++ src/app/store/actions.ts
@@ -4,3 +4,7 @@ import { createAction, props } from "@ngrx/store";
 export const deleteBeerAction = createAction(
   '[beer] deleteBeer',
  props<{ id : number}>());
+
+ export interface deleteBeerActionProps {
+
+ }
--- src/app/store/reducers.ts
+++ src/app/store/reducers.ts
@@ -1,6 +1,20 @@
-import { createReducer } from "@ngrx/store";
-import { beersData } from "../data/beer-list";
-import { BeerState } from "../model/state";
+import { createReducer, on } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { beersData } from '../data/beer-list';
+import { BeerState } from '../model/state';
+import { deleteBeerAction } from './actions';
+import * as Actions from '../store/actions';
 
-const initialBeerState: BeerState = {list: beersData};
-export const beerReducers = createReducer(initialBeerState);
+const initialBeerState: BeerState = { list: beersData };
+
+// export const beerReducers = createReducer(initialBeerState);
+
+export const deleteReduce = createReducer(
+  initialBeerState,
+
+  on(Actions.deleteBeerAction, (stateBeers, arg: { id: number }) => ({
+    ...stateBeers,
+
+    list: stateBeers.list.filter((beer) => beer.id != arg.id),
+  }))
+);
