5ff5e9f20ae6719625ae39c75f710cb9c6b6513c
ffiorentino12 <francesco.fiorentino@finconsgroup.com>
2022-07-21 17:41:50 +0200

    1 file modified

--- src/app/store/reducers.ts
+++ src/app/store/reducers.ts
@@ -10,24 +10,20 @@ const initialBeerState: BeerState = { list: beersData };
 
 // export const beerReducers = createReducer(initialBeerState);
 
+// Posso avere solo un Reduce
 export const deleteReduce = createReducer(
   initialBeerState,
 
   on(Actions.deleteBeerAction, (stateBeers, arg: { id: number }) => ({
     ...stateBeers,
-
     list: stateBeers.list.filter((beer) => beer.id != arg.id),
   })),
+
   on(Actions.addBeerAction, (state: BeerState, arg: { beer: Beer }) => {
-
     //state.list.push(arg.beer);
-
     return {
-
       state,
-
       list: [...state.list, arg.beer],
-
     };
 
   })
